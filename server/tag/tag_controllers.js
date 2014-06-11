11d12234647295757f67f89e76dfa81b799b121a
willwclo <willwclo@gmail.com>
2014-06-11 11:43:23 -0700

    Modify tag schema based on Adrian's suggestions

--- server/tag/tag_controllers.js
+++ server/tag/tag_controllers.js
@@ -4,28 +4,9 @@ var Tag = require('./tag_model.js');
 
 module.exports = exports = {
   post: function (req, res) {
-    var message = req.body.text;
-    var senderId = req.body.from;
-    var receiverName = req.body.to;
-
-    User.findOne({name: receiverName})
-      .exec(function (err, receiver) {
-        if (!err) {
-          new Tag({
-            from: senderId, 
-            to: receiver._id,
-            text: message
-          })
-          .save(function (err, message) {
-            if (err) { 
-              res.send(500, "Error in saving message to database."); 
-            } else {
-              res.send(201, message._id);
-            }
-          });
-        } else {
-          res.send(404, "Receiver not found");
-        }
-      });
+    res.send(200); // return nothing for now
+  },
+  get: function (req, res) {
+    res.send(200); // return nothing for now
   }
 };
--- server/tag/tag_model.js
+++ server/tag/tag_model.js
@@ -4,6 +4,8 @@ var mongoose = require('mongoose');
 
 var tagSchema = mongoose.Schema({
   name: {type: String, required: true, unique: true, index: true},
+  label: {type: String, required: true},
+  scaleDescription: [{type: String}],
   createdAt: {type: Date, default: Date.now},
   updatedAt: {type: Date, default: Date.now}
 });

7f1a19dcd7b3d35a2e7cd9aed41f74978ee58c8d
Jeffrey Gho <jeffreygho@gmail.com>
2014-06-11 16:42:43 -0700

    User routes and contollers

--- server/company/company_controllers.js
+++ server/company/company_controllers.js
@@ -4,10 +4,44 @@ var Company = require('./company_model.js');
 
 module.exports = exports = {
 
+  getById: function (req, res, next) {
+    Company.findById(req.body.id, function (err, company) {
+      if (err) {
+        res.send(500, err);
+        return;
+      }
+      res.json(200, company);
+    });
+  },
+
+  putById: function (req, res, next) {
+    Company.findById(req.body.id, function (err, company) {
+      if (err) {
+        res.send(500, err);
+        return;
+      }
+      for (var field in Company.schema.paths) {
+        if ( (field !== '_id') && (field !== '__v') ) {
+          if (req.body[field] !== undefined) {
+            company[field] = req.body[field];
+          }
+        }
+      }
+      company.save(function (err, item) {
+        if (err) {
+          res.send(500, err);
+          return;
+        }
+        res.json(200, item.id);
+      });
+    });
+  },
+
   get: function (req, res, next) {
     Company.find(function (err, companies) {
       if (err) {
         res.send(500, err);
+        return;
       }
       res.json(200, companies);
     });
@@ -17,6 +51,7 @@ module.exports = exports = {
     Company.create(req.body.company, function (err, company) {
       if (err) {
         res.send(500, err);
+        return;
       }
       res.send(201, company.id);
     });
--- server/company/company_routes.js
+++ server/company/company_routes.js
@@ -4,6 +4,10 @@ var controller = require('./company_controllers.js');
 
 module.exports = exports = function (router) {
 
+  router.route('/:id')
+    .get(controller.getById)
+    .put(controller.putById);
+
   router.route('/')
     .get(controller.get)
     .post(controller.post);

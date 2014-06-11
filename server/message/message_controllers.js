94a4632e7c330f5c444db458f001a7a4c3ba8186
willwclo <willwclo@gmail.com>
2014-06-11 11:06:46 -0700

    Fix indentation and variable naming in message controller

--- server/message/message_controllers.js
+++ server/message/message_controllers.js
@@ -6,19 +6,20 @@ module.exports = exports = {
   post: function (req, res, next) {
     var message = req.body.text;
     var senderId = req.body.from;
+    var receiverName = req.body.to;
 
-    User.findOne({name: req.body.to})
+    User.findOne({name: receiverName})
       .exec(function (err, receiver) {
         new Message({
           from: senderId, 
           to: receiver._id,
-          text: req.body.text
+          text: message
         })
         .save(function (err, message) {
           if (err) { 
             res.send(501, err); 
           } else {
-            res.send(200, message._id);
+            res.send(201, message._id);
           }
         });
       });

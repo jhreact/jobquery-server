2e27264794daf5e32f91d15fe11f70700fec89ab
Jeffrey Gho <jeffreygho@gmail.com>
2014-06-11 11:43:12 -0700

    Add links and questions to opportunity schema

--- server/opportunity/opportunity_model.js
+++ server/opportunity/opportunity_model.js
@@ -4,26 +4,36 @@ var mongoose = require('mongoose');
 
 var opportunitySchema = mongoose.Schema({
   company: {type: mongoose.Schema.Types.ObjectId, ref: 'Company', required: true},
-  tags: 
+  tags:
     [{
       tagId: {type: mongoose.Schema.Types.ObjectId, ref: 'Tag'},
       score: {type: Number, min: 1, max: 4}
     }],
   active: {type: Boolean, default: true, required: true},
   title: {type: String, required: true},
+  description: {type: String, required: true},
+  links:
+    [{
+      date: {type: Date, default: Date.now},
+      uri: String
+    }],
   notes:
     [{
       date: {type: Date, default: Date.now},
       text: String
     }],
-  internalNotes: 
+  internalNotes:
     [{
       date: {type: Date, default: Date.now},
       text: String
     }],
-  description: {type: String, required: true},
+  questions:
+    [{
+      date: {type: Date, default: Date.now},
+      question: String
+    }],
   createdAt: {type: Date, default: Date.now},
   updatedAt: {type: Date, default: Date.now}
 });
 
-module.exports = exports = mongoose.model('Opportunity', opportunitySchema); 
+module.exports = exports = mongoose.model('Opportunity', opportunitySchema);

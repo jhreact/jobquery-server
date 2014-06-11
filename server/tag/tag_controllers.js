49f717a0e580f459db3096432d6c7b6f6d029280
willwclo <willwclo@gmail.com>
2014-06-11 12:15:19 -0700

    Create post controller for tag

--- server/tag/tag_controllers.js
+++ server/tag/tag_controllers.js
@@ -4,13 +4,25 @@ var Tag = require('./tag_model.js');
 
 module.exports = exports = {
   post: function (req, res) {
-    res.send(200); // return nothing for now
+    Tag.create({
+      name: req.body.name,
+      label: req.body.label,
+      scaleDescription: req.body.scaleDescription || []
+    }), function (err, tag) {
+      if (err) { res.send(500, "Server unable to save tag."); }
+      res.send(201, tag._id);
+    });
   },
   get: function (req, res) {
     Tag.find({})
       .exec(function(err, tags) {
         if (err) { res.send(500, "Server unable to read tags from database."); }
-        res.send(tags);
+        res.send(200, tags);
       });
   }
 };
+
+
+name
+label
+scaleDescription

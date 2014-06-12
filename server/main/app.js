b10ce73fe0964502914eca92ffbf34e190394df1
Jeffrey Gho <jeffreygho@gmail.com>
2014-06-11 23:14:48 -0700

    Add routers info to config files

--- server/main/app.js
+++ server/main/app.js
@@ -1,33 +1,35 @@
 'use strict';
 
 var express      = require('express');
-
 var app          = express();
 
 /* Router */
 var NoteRouter        = express.Router();
-var MessageRouter     = express.Router();
-
 var LoginRouter       = express.Router();
+var MessageRouter     = express.Router();
 var OpportunityRouter = express.Router();
 var TagRouter         = express.Router();
 var UserRouter        = express.Router();
+var CompanyRouter     = express.Router();
 
 var routers      = {};
 
 routers.NoteRouter        = NoteRouter;
-routers.MessageRouter     = MessageRouter;
 routers.LoginRouter       = LoginRouter;
+routers.MessageRouter     = MessageRouter;
 routers.OpportunityRouter = OpportunityRouter;
 routers.TagRouter         = TagRouter;
 routers.UserRouter        = UserRouter;
+routers.CompanyRouter     = CompanyRouter;
 
 require('./config.js')(app, express, routers);
 
-require('../login/login_routes.js')(LoginRouter);
 require('../note/note_routes.js')(NoteRouter);
+require('../login/login_routes.js')(LoginRouter);
 require('../message/message_routes.js')(MessageRouter);
+require('../opportunity/opportunity_routes.js')(OpportunityRouter);
 require('../tag/tag_routes.js')(TagRouter);
 require('../user/user_routes.js')(UserRouter);
+require('../company/company_routes.js')(CompanyRouter);
 
 module.exports = exports = app;

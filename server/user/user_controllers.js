ebc67c878b8b7653743ad2e0416a9c5c245206e1
Jeffrey Gho <jeffreygho@gmail.com>
2014-06-11 17:04:03 -0700

    User routes and controller

--- server/user/user_controllers.js
+++ server/user/user_controllers.js
@@ -4,10 +4,44 @@ var User = require('./user_model.js');
 
 module.exports = exports = {
 
+  getById: function (req, res, next) {
+    User.findById(req.body.id, function (err, user) {
+      if (err) {
+        res.send(500, err);
+        return;
+      }
+      res.json(200, user);
+    });
+  },
+
+  putById: function (req, res, next) {
+    User.findById(req.body.id, function (err, user) {
+      if (err) {
+        res.send(500, err);
+        return;
+      }
+      for (var field in User.schema.paths) {
+        if ( (field !== '_id') && (field !== '__v') ) {
+          if (req.body[field] !== undefined) {
+            user[field] = req.body[field];
+          }
+        }
+      }
+      user.save(function (err, item) {
+        if (err) {
+          res.send(500, err);
+          return;
+        }
+        res.json(200, item.id);
+      });
+    });
+  },
+
   get: function (req, res, next) {
     User.find(function (err, users) {
       if (err) {
         res.send(500, err);
+        return;
       }
       res.json(200, users);
     });
@@ -17,6 +51,7 @@ module.exports = exports = {
     User.create(req.body.user, function (err, user) {
       if (err) {
         res.send(500, err);
+        return;
       }
       res.send(201, user.id);
     });
